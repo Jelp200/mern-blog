28faa3541c79fbd94398bf4327e9658856abd9e1
Jelp200 <jorge.pena0516@gmail.com>
2025-04-26 20:00:38 -0600

    :sparkles: [ADD] CONEXION A LA BASE DE DATOS MONGODB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,21 @@
 //* IMPORTACIONES
-import express from 'express'; // Importamos express
+import express from 'express';      // Import de express
+import mongoose from 'mongoose';    // Import de mongoose
+import dotenv from 'dotenv';        // Import de dotenv para manejar variables de entorno
+
+//* CONFIGURACIÓN DE VARIABLES DE ENTORNO
+dotenv.config(); // Cargar las variables de entorno desde el archivo .env
+
+mongoose
+    .connect(process.env.MONGODB_URI) // Conexión a la base de datos MongoDB usando la URI almacenada en las variables de entorno
+    .then(() => {
+        // Callback para confirmar la conexión a la base de datos
+        console.log('Conectado a MongoDB');
+    })
+    .catch((error) => {
+        // Callback para manejar errores de conexión
+        console.error('Error al conectar a MongoDB:', error);
+    });
 
 // Creamos una instancia de express
 const app = express();
