043d23398dc5ee9d4f761b2b874382519960405b
Jelp200 <jorge.pena0516@gmail.com>
2025-04-26 20:10:03 -0600

    :sparkles: [ADD] CREACION DEL MODELO DEL USUARIO

---
+++ api/models/user.model.js
@@ -0,0 +1,32 @@
+//* IMPORTS
+import mongoose from 'mongoose'; // Import de mongoose para crear el modelo de usuario
+
+//* SCHEMA
+// Definición del esquema de usuario
+const userSchema = new mongoose.Schema({
+    // Definición de los campos del esquema
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    // Se añade la opción de timestamps para crear los campos createdAt y updatedAt automáticamente
+}, { timestamps: true }
+);
+
+//* MODEL
+// Creación del modelo de usuario a partir del esquema definido anteriormente
+const User = mongoose.model('User', userSchema);
+
+//* EXPORTS
+// Exportación del modelo de usuario para poder utilizarlo en otras partes de la aplicación
+export default User;
