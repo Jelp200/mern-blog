d851d5635ca92bd8a5626a07e087ccd4bb818d7e
Jelp200 <jorge.pena0516@gmail.com>
2025-04-30 19:19:40 -0600

    :sparkles: [ADD] REDUX PERSIST

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice'
+import { configureStore, combineReducers } from '@reduxjs/toolkit' // Importa las funciones de configuración de storage y combinación de reducers
+import userReducer from './user/userSlice' // Importa el slice de usuario
+import { persistReducer, persistStore } from 'redux-persist' // Importa la función persistReducer
+import storage from 'redux-persist/lib/storage' // Importa el almacenamiento
+
+const rootReducer = combineReducers({
+    user: userReducer,  // Reducer para el slice de usuario
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1, // Versión del estado persistido
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer,  // Reducer para el slice de usuario
-    },
-})
+    reducer: persistedReducer, // Reducer persistido
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }), // Middleware para manejar la serialización
+});
+
+export const persistor = persistStore(store) // Crea el persistor para el almacenamiento persistente
