b8169fb4624bdd35ec1dddab3aa7d5f32e3e74ba
Jelp200 <jorge.pena0516@gmail.com>
2025-05-05 23:08:15 -0600

    :sparkles: [ADD] SECCION DE POST AL DASHBOARD

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { Sidebar, SidebarItem, SidebarItemGroup, SidebarItems } from 'flowbite-react'
-import { HiUser, HiArrowSmRight } from 'react-icons/hi'
+import { HiUser, HiArrowSmRight, HiDocumentText } from 'react-icons/hi'
 import { signoutSuccess } from '../redux/user/userSlice'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 export default function DashSidebar() {
     const location = useLocation();
-    const [tab, setTab] = useState('');
     const dispatch = useDispatch();
+    const {currentUser} = useSelector((state) => state.user);
+    const [tab, setTab] = useState('');
+    
 
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
@@ -41,12 +43,12 @@ export default function DashSidebar() {
         <div className="h-full">
             <Sidebar className="w-full md:w-56 ">
                 <SidebarItems>
-                    <SidebarItemGroup>
+                    <SidebarItemGroup className='flex flex-col gap-1'>
                         <Link to='/dashboard?tab=profile'>
                             <SidebarItem
                                 active={tab === 'profile'}
                                 icon={HiUser}
-                                label={'User'}
+                                label={currentUser.isAdmin ? 'Admin' : 'User'}
                                 labelColor='dark'
                                 as='div'
                             >
@@ -54,6 +56,20 @@ export default function DashSidebar() {
                                 Profile
                             </SidebarItem>
                         </Link>
+
+                        {currentUser.isAdmin && (
+                            <Link to='/dashboard?tab=posts'>
+                                <SidebarItem
+                                    active={tab === 'posts'}
+                                    icon={HiDocumentText}
+                                    as='div'
+                                >
+
+                                    Posts
+                                </SidebarItem>
+                            </Link>
+                        )}
+
                         <SidebarItem
                             icon={HiArrowSmRight}
                             className='cursor-pointer'
