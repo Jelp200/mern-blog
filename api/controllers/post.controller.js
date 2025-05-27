b23b8ebfb92e42fb8178bd5506cd5eba8e2ad412
Jelp200 <jorge.pena0516@gmail.com>
2025-05-27 16:18:51 -0600

    :sparkles: [ADD] CREACION DE LA API PARA OBTENER POSTS

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -26,3 +26,42 @@ export const create = async (req, res, next) => {
         next(error);
     }
 };
+
+export const getposts = async(req, res, next) => {
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;             // Obtener el índice de inicio desde la consulta, o 0 por defecto
+        const limit = parseInt(req.query.limit) || 9;                       // Limite de la consulta
+        const sortDirection = req.query.order === 'asc' ? 1 : -1;           // Orden ascendente o descendente
+        const posts = await Post.find({
+            ...(req.query.userId && { userId: req.query.userId }),          // Filtrar por userId si se proporciona
+            ...(req.query.category && { category: req.query.category }),    // Filtrar por category si se proporciona
+            ...(req.query.slug && { category: req.query.slug }),            // Filtrar por slug si se proporciona
+            ...(req.query.postId && { _id: req.query.postId }),             // Filtrar por postId si se proporciona
+            ...(req.query.searchTerm && {                                   // Filtrar por searchTerm si se proporciona
+                $or: [
+                    { title: { $regex: req.query.searchTerm, $options: 'i' } },     // Búsqueda en el título
+                    { content: { $regex: req.query.searchTerm, $options: 'i' } }    // Búsqueda en el contenido
+                ],
+            }),
+        }).sort({ updateAt: sortDirection }).skip(startIndex).limit(limit); // Aplicar ordenamiento, paginación y límite
+
+        const totalPost = await Post.countDocuments();                      // Contar el total de documentos
+        const now = new Date();
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate()
+        );
+        const lastMonthPosts = await Post.countDocuments({
+            createdAt: { $gte: oneMonthAgo },
+        });
+
+        res.status(200).json({
+            posts,
+            totalPost,
+            lastMonthPosts,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,12 @@
 //* IMPORTS
 import express from 'express';                              // Import de express
 import { verifyToken } from '../utils/verifyUser.js';
-import { create } from '../controllers/post.controller.js';
+import { create, getposts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
-router.post('/create', verifyToken, create);
+router.post('/create', verifyToken, create);                // Ruta para crear un post, requiere verificación de token
+router.get('/getposts', getposts);                          // Ruta para obtener un post, no requiere verificacion de token
 
 // Exportar el enrutador para usarlo en otras partes de la aplicación
 export default router;
