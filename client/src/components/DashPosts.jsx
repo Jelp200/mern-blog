b4f5d699382eb56737e344fbff24a205268c0ad4
Jelp200 <jorge.pena0516@gmail.com>
2025-05-28 11:34:08 -0600

    :sparkles: [ADD] FUNCIONABILIDAD DE SHOWMORE

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,11 +1,12 @@
 import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from 'flowbite-react';
 import { Link } from 'react-router-dom';
-import { useEffect, useState } from "react"
-import { useSelector } from 'react-redux'
+import { useEffect, useState } from "react";
+import { useSelector } from 'react-redux';
 
 export default function DashPosts() {
     const { currentUser } = useSelector((state) => state.user);
     const [userPosts, setUserPosts] = useState([]);
+    const [showMore, setShowMore] = useState(true);
     console.log(userPosts);
     useEffect(() => {
         const fetchPosts = async () => {
@@ -14,6 +15,9 @@ export default function DashPosts() {
                 const data = await res.json();
                 if (res.ok) {
                     setUserPosts(data.posts);
+                    if (data.post.length < 9) {
+                        setShowMore(false);
+                    }
                 }
             } catch (error) {
                 console.error(error.message);
@@ -24,63 +28,89 @@ export default function DashPosts() {
         }
     }, [currentUser._id]);
 
-    return <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
-        {currentUser.isAdmin && userPosts.length > 0 ? (
-            <>
-                <Table hoverable className='shadow-md'>
-                    <TableHead>
-                        <TableHeadCell>Date updated</TableHeadCell>
-                        <TableHeadCell>Post image</TableHeadCell>
-                        <TableHeadCell>Post title</TableHeadCell>
-                        <TableHeadCell>Category</TableHeadCell>
-                        <TableHeadCell>Delete</TableHeadCell>
-                        <TableHeadCell>
-                            <span>Edit</span>
-                        </TableHeadCell>
-                    </TableHead>
-                    {userPosts.map((post) => (
-                        <TableBody className='divide-y'>
-                            <TableRow className='bg-white dark:border-gray-700 dark:bg-gray-800'>
-                                <TableCell>
-                                    {new Date(post.updatedAt).toLocaleDateString()}
-                                </TableCell>
+    const handleShowMore = async () => {
+        const startIndex = userPosts.length;
+        try {
+            const res = await fetch(`/api/post/getposts?userId=${currentUser._id}$startIndex=${startIndex}`);
+            const data = await res.json();
+            if (res.ok) {
+                setUserPosts((prev) => [...prev, ...data.posts]);
+                if (data.posts.length < 9) {
+                    setShowMore(false);
+                }
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
 
-                                <TableCell>
-                                    <Link to={`/post/${post.slug}`}>
-                                        <img
-                                            src={post.image}
-                                            alt={post.title}
-                                            className='w-20 h-10 object-cover bg-gray-500'
-                                        />
-                                    </Link>
-                                </TableCell>
+    return (
+        <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+            {currentUser.isAdmin && userPosts.length > 0 ? (
+                <>
+                    <Table hoverable className='shadow-md'>
+                        <TableHead>
+                            <TableHeadCell>Date updated</TableHeadCell>
+                            <TableHeadCell>Post image</TableHeadCell>
+                            <TableHeadCell>Post title</TableHeadCell>
+                            <TableHeadCell>Category</TableHeadCell>
+                            <TableHeadCell>Delete</TableHeadCell>
+                            <TableHeadCell>
+                                <span>Edit</span>
+                            </TableHeadCell>
+                        </TableHead>
+                        {userPosts.map((post) => (
+                            <TableBody className='divide-y'>
+                                <TableRow className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                                    <TableCell>
+                                        {new Date(post.updatedAt).toLocaleDateString()}
+                                    </TableCell>
 
-                                <TableCell>
-                                    <Link className='font-medium text-gray-900 dark:text-white' to={`/post/${post.slug}`}>
-                                        {post.title}
-                                    </Link>
-                                </TableCell>
+                                    <TableCell>
+                                        <Link to={`/post/${post.slug}`}>
+                                            <img
+                                                src={post.image}
+                                                alt={post.title}
+                                                className='w-20 h-10 object-cover bg-gray-500'
+                                            />
+                                        </Link>
+                                    </TableCell>
 
-                                <TableCell>
-                                    {post.category}
-                                </TableCell>
+                                    <TableCell>
+                                        <Link className='font-medium text-gray-900 dark:text-white' to={`/post/${post.slug}`}>
+                                            {post.title}
+                                        </Link>
+                                    </TableCell>
 
-                                <TableCell>
-                                    <span className='font-medium text-red-500 hover:underline cursor-pointer'>Delete</span>
-                                </TableCell>
+                                    <TableCell>
+                                        {post.category}
+                                    </TableCell>
 
-                                <TableCell>
-                                    <Link className='text-teal-500 hover:underline' to={`/update-post/${post._id}`}>
-                                        <span>Edit</span>
-                                    </Link>
-                                </TableCell>
-                            </TableRow>
-                        </TableBody>
-                    ))}
-                </Table>
-            </>
-        ) : (
-            <p>You have no post yet...</p>
-        )}
-    </div>;
+                                    <TableCell>
+                                        <span className='font-medium text-red-500 hover:underline cursor-pointer'>Delete</span>
+                                    </TableCell>
+
+                                    <TableCell>
+                                        <Link className='text-teal-500 hover:underline' to={`/update-post/${post._id}`}>
+                                            <span>Edit</span>
+                                        </Link>
+                                    </TableCell>
+                                </TableRow>
+                            </TableBody>
+                        ))}
+                    </Table>
+
+                    {
+                        showMore && (
+                            <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7 hover:underline cursor-pointer'>
+                                Show more
+                            </button>
+                        )
+                    }
+                </>
+            ) : (
+                <p>You have no post yet...</p>
+            )}
+        </div>
+    );
 }
