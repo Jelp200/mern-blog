73063974ac1ae55d7dfe5ff9ebe0250e9d12cee9
Jelp200 <jorge.pena0516@gmail.com>
2025-05-28 15:22:01 -0600

    :sparkles: [ADD] API PARA OBTENER USUSARIOS

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 //* IMPORTS
 import express from 'express';
-import { test, updateUser, deleteUser, signout } from '../controllers/user.controller.js'; // Import de la función de prueba del controlador de usuario
+import { test, updateUser, deleteUser, signout, getUsers } from '../controllers/user.controller.js'; // Import de la función de prueba del controlador de usuario
 import { verifyToken } from '../utils/verifyUser.js';
 
 // Crear un enrutador de Express
@@ -10,6 +10,7 @@ router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
+router.get('/getusers', verifyToken, getUsers);
 
 // Exportar el enrutador para usarlo en otras partes de la aplicación
 export default router;
