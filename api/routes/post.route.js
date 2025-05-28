5f09b9725324588dbb6e429f860eca8a7df5e4f6
Jelp200 <jorge.pena0516@gmail.com>
2025-05-28 12:09:49 -0600

    :sparkles: [ADD] FUNCIONALIDAD DE POST DELETE

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,12 +1,13 @@
 //* IMPORTS
 import express from 'express';                              // Import de express
 import { verifyToken } from '../utils/verifyUser.js';
-import { create, getposts } from '../controllers/post.controller.js';
+import { create, getposts, deletepost } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create);                // Ruta para crear un post, requiere verificación de token
 router.get('/getposts', getposts);                          // Ruta para obtener un post, no requiere verificacion de token
+router.delete('/deletepost/:postId/:userId', verifyToken, deletepost);  // Ruta para eliminar un post, requiere verificación de token
 
 // Exportar el enrutador para usarlo en otras partes de la aplicación
 export default router;
