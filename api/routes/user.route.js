f9a65f3210da980f84d1327d6fb7cf68c292d8b8
Jelp200 <jorge.pena0516@gmail.com>
2025-06-03 12:38:28 -0600

    :sparkles: [ADD] VISUALIZACION DE LOS COMENTARIOS DEL POST

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 //* IMPORTS
 import express from 'express';
-import { test, updateUser, deleteUser, signout, getUsers } from '../controllers/user.controller.js'; // Import de la función de prueba del controlador de usuario
+import { test, updateUser, deleteUser, signout, getUsers, getUser } from '../controllers/user.controller.js'; // Import de la función de prueba del controlador de usuario
 import { verifyToken } from '../utils/verifyUser.js';
 
 // Crear un enrutador de Express
@@ -11,6 +11,7 @@ router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
 router.get('/getusers', verifyToken, getUsers);
+router.get('/:userId', getUser);
 
 // Exportar el enrutador para usarlo en otras partes de la aplicación
 export default router;
